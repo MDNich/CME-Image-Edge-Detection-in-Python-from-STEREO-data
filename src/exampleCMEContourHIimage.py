bcab52d4462e394a3eb59e8609275b7b532d2b3e
Astro Kid <71262643+MDNich@users.noreply.github.com>
2022-06-07 09:56:48 -0400

    update

--- src/exampleCMEContourHIimage.py
+++ src/exampleCMEContourHIimage.py
@@ -4,10 +4,12 @@ from CMEContourHIimage import CMEContourHIimage
 hiCME = CMEContourHIimage()
 
 hiCME.loadImages("inputData/20081212_172901_2bh1B_br11.fts","inputData/20081212_180901_2bh1B_br11.fts","b")
+#hiCME.loadImages("inputData/20081212_152901_2bh1B_br11.fts","inputData/20081212_160901_2bh1B_br11.fts","b")
 
 hiCME.processCMEContour(True)
 
 hiCME.drawCMEContour() # provide a string to the path where the image should 
                        # be saved if you want to save it and not just show.
 
-
+# Comment this if you do not want to see intermediate steps
+hiCME.drawProcDetail()
